a257fa85baab82ca0b71d29d32e6454c535efeec
Willem <94999824+OmeWillem@users.noreply.github.com>
2025-08-18 23:04:37 +0200

    fix: probably not important

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsConfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   server: {
     port: 3000,
-    allowedHosts: ["panel.dustydunes.net"],
+    allowedHosts: ["dash.mineplay.nl"],
   },
   plugins: [
     tsConfigPaths({
